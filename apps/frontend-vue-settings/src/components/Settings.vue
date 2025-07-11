046d27adf0a4cdb2517e6ca0a588ac1ca844485b
pickle99 <jackrestler@gmail.com>
2025-07-12 01:34:19 +0400

    feat: remove unused alert component, add padding to hide whitespace

--- apps/frontend-vue-settings/src/components/Settings.vue
+++ apps/frontend-vue-settings/src/components/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="settings-page cf" style="padding: 1.5rem">
+  <div class="settings-page cf" style="padding: 1.5rem; padding-bottom: 10rem">
     <div
       @click="handleRedirectToStore"
       style="display: flex; align-items: center; gap: 0.7rem; cursor: pointer; width: fit-content"
@@ -17,31 +17,6 @@
           updated products are visible by default for customers, export the products to CSV/XSLX, or
           enable/disable the widget. P.S: this page is built for store owner, however right now it
           does not have any auth or guard.
-        </div>
-      </div>
-
-      <div class="alert alert-success alert-icon">
-        <div class="alert-inner">
-          <div class="alert-content">
-            <div class="icon">
-              <span class="svg-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70">
-                  <path
-                    d="M34.5 67h-.13c-8.68-.03-16.83-3.45-22.94-9.61C5.32 51.23 1.97 43.06 2 34.38 2.07 16.52 16.65 2 34.5 2h.13c8.68.03 16.83 3.45 22.94 9.61 6.12 6.16 9.46 14.34 9.43 23.02C66.93 52.48 52.35 67 34.5 67zm0-62C18.3 5 5.06 18.18 5 34.39c-.03 7.88 3.01 15.3 8.56 20.89 5.55 5.59 12.95 8.69 20.83 8.72h.12c16.2 0 29.44-13.18 29.5-29.39.03-7.88-3.01-15.3-8.56-20.89C49.89 8.13 42.49 5.03 34.61 5h-.11z"
-                  ></path>
-                  <path
-                    d="M32.17 46.67l-10.7-10.08c-.6-.57-.63-1.52-.06-2.12.57-.6 1.52-.63 2.12-.06l8.41 7.92 14.42-16.81c.54-.63 1.49-.7 2.12-.16.63.54.7 1.49.16 2.12L32.17 46.67z"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-            <div class="title">This is an example alert you can show in your app.</div>
-            <div>
-              Use the alert messages to catch users’ attention. If you want to let your users know
-              about a problem or successful operation in your app, use the alerts.
-              <a onclick="postOpenPage('#components.alerts')">See Alerts</a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
